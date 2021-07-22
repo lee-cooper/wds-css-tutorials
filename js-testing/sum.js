943bbfc217371406af7bcac97ca4467c7d678e9e
Lee Cooper <cooperlee372@gmail.com>
2021-07-21 20:40:47 -0400

    basic unit testing with jest

---
+++ js-testing/sum.js
@@ -0,0 +1,11 @@
+function sum(a, b) {
+  return a + b;
+}
+
+// Not tested, shows that not all lines are covered by tests
+
+function helper() {
+  console.log("helper");
+}
+
+module.exports = sum;
